0b3ec0cbc76219033823523bc974f980025ff1b9
Cainuriel <cainuriel@gmail.com>
2020-09-22 16:22:36 +0200

    Modal en login

--- www/login.php
+++ www/login.php
@@ -14,7 +14,7 @@ if (isset($_GET['comilon'])) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Cartas QR - Admin</title>
+    <title>Admin</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
@@ -42,7 +42,7 @@ if (isset($_GET['comilon'])) {
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <img src="admin/img/comilones_logo.png" alt="logo de comilones">
+            <a href="index.php"> <img src="admin/img/comilones_logo.png" alt="logo de comilones"></a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
@@ -70,7 +70,7 @@ if (isset($_GET['comilon'])) {
 
                         <!-- /.col -->
                         <div class="col">
-                            <button data-toggle="modal" data-target="#staticBackdrop" class="btn btn-primary btn-block">Acceder</button>
+                            <a data-toggle="modal" data-target="#staticBackdrop" class=" btn btn-primary btn-block text-white text-bold">Acceder</a>
                         </div>
 
                         <!-- /.col -->
@@ -83,8 +83,8 @@ if (isset($_GET['comilon'])) {
                 // $login->loginUsuarioCtr();
                 ?>
 
-                <p class="mb-1 text-center mt-3"><a href="#" data-toggle="modal" data-target="#staticBackdrop">¿Ha olvidado la contraseña?</a></p>
-                <p class="mb-1 text-center mt-3"><a href="#" data-toggle="modal" data-target="#staticBackdrop">¿No tiene cuenta? Registrese</a></p>
+                <p class="mb-1 text-center mt-3"><a href="" data-toggle="modal" data-target="#staticBackdrop">¿Ha olvidado la contraseña?</a></p>
+                <p class="mb-1 text-center mt-3"><a href="" data-toggle="modal" data-target="#staticBackdrop">¿No tiene cuenta? Registrese</a></p>
 
 
                 <!-- Modal -->
@@ -134,8 +134,10 @@ if (isset($_GET['comilon'])) {
         </div>
         <!-- /warning -->
 
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-    <?php } // no cerramos el php para que no se introduzca posible codigo malicioso
+
+    <?php } ?>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
